3b8efa75a059b7ba22f8c0653c6dfdd61c46d5b7
yadav-deepakk <deepakyadav53882@gmail.com>
2024-03-20 23:49:20 +0530

    taking pathvariable and using it in function.

--- src/main/java/com/example/springboot/project/controller/GreetController.java
+++ src/main/java/com/example/springboot/project/controller/GreetController.java
@@ -1,14 +1,21 @@
 package com.example.springboot.project.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetController {
 
 	@GetMapping("/home")
-	public String HelloWorld() {
+	public String helloWorld() {
 		return "<H2 align='center'>Hello World!<H2>";
 	}
+	
+	@GetMapping("/greetuser/{userName}")
+	public String greetUser(@PathVariable String userName) {
+		return "<H2 align='center'>Hello " + userName + "<H2>"; 
+	}
+
 
 }
