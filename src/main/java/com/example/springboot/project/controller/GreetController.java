2cd1b46f8048ece058a7ea0ac5c506d346d86edb
yadav-deepakk <deepakyadav53882@gmail.com>
2024-03-21 00:00:01 +0530

    taking request param and using it in function.

--- src/main/java/com/example/springboot/project/controller/GreetController.java
+++ src/main/java/com/example/springboot/project/controller/GreetController.java
@@ -2,6 +2,7 @@ package com.example.springboot.project.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,6 +17,10 @@ public class GreetController {
 	public String greetUser(@PathVariable String userName) {
 		return "<H2 align='center'>Hello " + userName + "<H2>"; 
 	}
-
+	
+	@GetMapping("/search")
+	public String search(@RequestParam(name = "q", required = true) String q) {
+		return "<h3 align='center'>Searching for " + q +"...</h3>"; 
+	}
 
 }
