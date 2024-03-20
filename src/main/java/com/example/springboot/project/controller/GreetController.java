f125c96c4f89f14b9d340b2a6e665ae73e808b0e
yadav-deepakk <deepakyadav53882@gmail.com>
2024-03-21 00:37:17 +0530

    taking post mapping and sending response entity.

--- src/main/java/com/example/springboot/project/controller/GreetController.java
+++ src/main/java/com/example/springboot/project/controller/GreetController.java
@@ -1,7 +1,11 @@
 package com.example.springboot.project.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,15 +16,21 @@ public class GreetController {
 	public String helloWorld() {
 		return "<H2 align='center'>Hello World!<H2>";
 	}
-	
+
 	@GetMapping("/greetuser/{userName}")
 	public String greetUser(@PathVariable String userName) {
-		return "<H2 align='center'>Hello " + userName + "<H2>"; 
+		return "<H2 align='center'>Hello " + userName + "<H2>";
 	}
-	
+
 	@GetMapping("/search")
 	public String search(@RequestParam(name = "q", required = true) String q) {
-		return "<h3 align='center'>Searching for " + q +"...</h3>"; 
+		return "<h3 align='center'>Searching for " + q + "...</h3>";
+	}
+
+	@PostMapping("/post")
+	public ResponseEntity<String> post(@RequestBody String requestBody) {
+		return new ResponseEntity<>("Received request with body: " + requestBody, HttpStatus.OK);
+
 	}
 
 }
